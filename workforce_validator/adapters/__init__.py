2cf67cc17d68a817e93af1911335251c97efd6fb
Aitorhp <aitorhp@inditex.com>
2026-08-19 13:24:49 +0200

    add source adapter package

---
+++ workforce_validator/adapters/__init__.py
@@ -0,0 +1,4 @@
+from workforce_validator.adapters.bundle import BundleAdapter
+from workforce_validator.adapters.current_json import CurrentJsonAdapter
+
+__all__ = ["BundleAdapter", "CurrentJsonAdapter"]
