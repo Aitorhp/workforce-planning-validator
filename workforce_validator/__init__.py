d4794233320db69accc82e694f71c53992058bf9
Aitorhp <aitorhp@inditex.com>
2026-08-19 13:25:50 +0200

    expose canonical validation api

--- workforce_validator/__init__.py
+++ workforce_validator/__init__.py
@@ -1,13 +1,21 @@
 from workforce_validator.config import SETTINGS, ValidatorSettings, load_settings
-from workforce_validator.engine import run_validation
-from workforce_validator.models import AbsenceDay, Incident, ShiftRow, ValidationResult
+from workforce_validator.engine import run_canonical_validation, run_validation
+from workforce_validator.models import (
+    AbsenceDay,
+    CanonicalDataset,
+    Incident,
+    ShiftRow,
+    ValidationResult,
+)
 
 __all__ = [
     "SETTINGS",
     "ValidatorSettings",
     "load_settings",
     "run_validation",
+    "run_canonical_validation",
     "AbsenceDay",
+    "CanonicalDataset",
     "Incident",
     "ShiftRow",
     "ValidationResult",
